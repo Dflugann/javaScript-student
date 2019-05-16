20aab6ed6652b7585cfe358b20d68605efe17398
Flavio <flavioanalistadesistema@gmail.com>
2019-05-16 17:31:50 -0300

    basic var, let, const

---
+++ exercicios-js/variaveiConstante.js
@@ -0,0 +1,19 @@
+var a = 3
+var a = 20 // evitar o uso (var)
+let b = 4  // regra padrao (let)
+
+var a = 3
+b = 2
+//let b = 2
+
+console.log(a, b)
+
+a= 300
+b= 200
+
+console.log(a, b)
+
+const c = 5 // variaveis que nao vai sofrer nudança
+//c =5 - constante nao pode ser alterado
+
+console.log(c)
