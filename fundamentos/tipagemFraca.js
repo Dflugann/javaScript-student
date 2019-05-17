afe961081b4ab33cde343c7e91618b80be592e39
Flavio <flavioanalistadesistema@gmail.com>
2019-05-17 10:44:24 -0300

    Tipagem Fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let number = 123
+console.log(number + '=' + typeof number)
+
+let floot = 12.345
+console.log(floot + '=' + typeof floot)
+
+let name = 'Flavio'
+console.log(name + '=' + typeof name )
+
+let status = true
+console.log(status + '=' + typeof status)
+
+//javaScript é uma linguagem fracamente tipada
