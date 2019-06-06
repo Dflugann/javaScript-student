b65f340ed8f36d69103264a1e07f6f2359c46b0e
Flavio <flavioanalistadesistema@gmail.com>
2019-06-06 11:11:08 -0300

    Usando template String

---
+++ fundamentos/templeteString.js
@@ -0,0 +1,12 @@
+let nome = 'Flavio'
+let concatenando = 'Olá ' + nome + '!'
+
+console.log(concatenando)
+
+const template = `
+    ola ${nome}!
+`
+console.log(template)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei.... ${up('não faça isso')}!!!`)
