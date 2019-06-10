2f0e11d2e73dbeeadb009126a91ccc25cde698bc
Flavio <flavioanalistadesistema@gmail.com>
2019-06-10 11:48:04 -0300

    Instalando Apps && Scripts

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -3,7 +3,7 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const mulheres = f => f.genero === "F"
 const chinesas = f => f.pais === "China"
 const menorSalario = (func, funcAtual) => {
-    return func.salario > funcAtual.salario ? func : funcAtual
+    return func.salario < funcAtual.salario ? func : funcAtual
 }
 
 const axios = require(`axios`)
