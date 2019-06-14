aa49eff1aef84914f930dd685be6cecb8091153e
Flavio <flavioanalistadesistema@gmail.com>
2019-06-14 20:04:58 -0300

    Object javaScript

---
+++ fundamentos/objects.js
@@ -0,0 +1,19 @@
+const prod1 = {}
+prod1.name = "Tablet Sansung",
+prod1.preco= 966.00
+prod1['desconto'] = 0.6
+
+console.log(prod1)
+
+const prod2 = {
+    name: 'Mi band4',
+    preco: 200.99,
+     obj : {
+         modelo: "relogio de pulso",
+         obj : {
+             subModelo: "prova d'agua"
+         }
+     }
+}
+
+console.log(prod2);
