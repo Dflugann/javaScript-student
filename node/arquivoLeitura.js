db8e9f59159027a1cadc7fc0a06061eff732d2bf
Flavio <flavioanalistadesistema@gmail.com>
2019-06-14 19:28:05 -0300

    Update - Ler arquivo

--- node/arquivoLeitura.js
+++ node/arquivoLeitura.js
@@ -1,11 +1,24 @@
-const fs = require('fs')
-const caminho = __dirname + '/arquivo.json'
+const fs = require('fs');
 
-//sincrono..
-const arq = fs.readFileSync(caminho, 'utf-8')
-// console.log(arq);
+const caminho = __dirname + '/arquivo.json';
 
-fs.readFile(caminho, 'utf-8', (err, conteudo) => {
-    const config = JSON.parse(conteudo)
-    console.log(config.node.user)
+const arquivo = fs.readFileSync(caminho, 'utf-8');
+console.log(arquivo);
+
+fs.readFile(caminho, 'utf-8', (err, sucess) => {
+    if (err !== undefined) {
+        LerArquivo = JSON.parse(sucess);
+        data = LerArquivo.node;
+        console.log(`${data.host}:${data.port}`)
+    }
 })
+
+fs.readdir(__dirname, (err, arquivo) => {
+    if (err !== undefined) {
+        console.log("Conteudo da pasta.....");
+        console.log(arquivo);
+    }
+})
+
+const lerJson = require(__dirname + '/arquivo.json');
+console.log(lerJson.node.pass)
